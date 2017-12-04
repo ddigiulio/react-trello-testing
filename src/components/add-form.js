8554a8b3b106b40c65bd62c15ec9584e27009546
ddigiulio <ddigiulio90@gmail.com>
2017-12-04 11:12:46 -0500

    fixed issue

--- src/components/add-form.js
+++ src/components/add-form.js
@@ -30,7 +30,7 @@ export default class AddForm extends React.Component {
             return (
                 <div className="add-button"
                     onClick={() => this.setEditing(true)}>
-                    <a href="#">Add a {this.props.type}...</a>
+                    Add a {this.props.type}...
                 </div>
             );
         }
